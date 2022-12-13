9cb0bb4a9f719369d931498e205b5770ddcc2de5
Viki Val <viktorko99@gmail.com>
2022-12-13 12:25:37 +0100

    :squid: codegen

---
+++ src/model/generated/_interaction.ts
@@ -0,0 +1,10 @@
+export enum Interaction {
+    MINT = "MINT",
+    MINTNFT = "MINTNFT",
+    LIST = "LIST",
+    UNLIST = "UNLIST",
+    BUY = "BUY",
+    SEND = "SEND",
+    CONSUME = "CONSUME",
+    DESTROY = "DESTROY",
+}
