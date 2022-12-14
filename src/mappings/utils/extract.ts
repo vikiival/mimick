6964a084b28993716729048fafb4437de709e109
Viki Val <viktorko99@gmail.com>
2022-12-14 11:35:28 +0100

    😈 smol coding rave

--- src/mappings/utils/extract.ts
+++ src/mappings/utils/extract.ts
@@ -1,8 +1,9 @@
-import { ContractsContractEmittedEvent } from '@subsquid/substrate-processor'
-import { addressOf } from './helper';
+import { BatchBlock, ContractsContractEmittedEvent } from '@subsquid/substrate-processor'
+import { CONTRACT_ADDRESS } from '../../constants'
+import { decodeEvent, RealEvent } from './ink'
 import {
-  BaseCall, CallWith, Context, MetaContext, UnwrapFunc,
-} from './types';
+  BaseCall, Context, Processor
+} from './types'
 
 
 // function toBaseCall(extrinsic: ExtrinsicHandlerContext): BaseCall {
@@ -13,22 +14,46 @@ import {
 //   return { caller, blockNumber, timestamp };
 // }
 
-// function toBaseEvent(ctx: MetaContext): BaseCall {}
-function toBaseCall(ctx: Context): BaseCall {
-  return toBase(ctx, ctx.event);
+type BaseEvent = BaseCall & RealEvent;
+
+export function metaHandler(ctx: Context): BaseEvent[] {
+  return ctx.blocks.map(block => {
+    const baseBlock = toBase(block);
+    const events = enhanceItems(block.items, baseBlock);
+    return events;
+  }).flat();
 }
 
-function toBase(ctx: MetaContext, event: ContractsContractEmittedEvent): BaseCall {
+function enhanceItems(items: Processor[], baseBlock: BaseCall): BaseEvent[] {
+  // eslint-disable-next-line id-length
+  return items.map(toBaseEvent).filter(notEmpty).map(e => ({ ...baseBlock, ...e }));
+}
+
+function notEmpty<T>(value: T | null | undefined): value is T {
+  return value !== null && value !== undefined;
+}
+
+function toBase(ctx: BatchBlock<Processor>): BaseCall {
   // const caller = addressOf(event.extrinsic?.signature?.address.value);
   const caller = ''
-  const blockNumber = ctx.block.height.toString();
-  const timestamp = new Date(ctx.block.timestamp);
+  const blockNumber = ctx.header.height.toString();
+  const timestamp = new Date(ctx.header.timestamp);
 
   return { caller, blockNumber, timestamp };
 }
 
-export function unwrap<T>(ctx: Context, unwrapFn: UnwrapFunc<T>): CallWith<T> {
-  const baseCall = toBaseCall(ctx);
-  const unwrapped = unwrapFn(ctx);
-  return { ...baseCall, ...unwrapped };
+function toBaseEvent(ctx: Processor): RealEvent | null {
+  if (ctx.name === 'Contracts.ContractEmitted' && ctx.event.args.contract === CONTRACT_ADDRESS) {
+    const item = ctx.event as ContractsContractEmittedEvent;
+    const event = decodeEvent(item);
+    return event;
+  }
+
+  return null
 }
+
+// export function unwrap<T>(ctx: Context, unwrapFn: UnwrapFunc<T>): CallWith<T> {
+//   const baseCall = toBaseCall(ctx);
+//   const unwrapped = unwrapFn(ctx);
+//   return { ...baseCall, ...unwrapped };
+// }
