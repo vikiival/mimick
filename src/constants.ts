cda64121bd133a754cfddf77a6f74750433282ec
Viki Val <viktorko99@gmail.com>
2022-12-14 11:28:30 +0100

    :wrench: CONTRACT_ADDRESS

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ import { lookupArchive } from '@subsquid/archive-registry'
 
 export const archive = lookupArchive('shibuya', { release: 'FireSquid' })
 export const STARTING_BLOCK = 2_790_000 // 6000 or 1790000 for Prod
+export const CONTRACT_ADDRESS = 'abXaTso17JvAekJoBYy3Aam92FQWxPsfxjag1fhncz2oraY'
