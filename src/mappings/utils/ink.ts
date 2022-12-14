643ed697d8fd2505ee7c2500918fe3a47926676a
Viki Val <viktorko99@gmail.com>
2022-12-14 16:19:03 +0100

    :zap: what is this transfer?

--- src/mappings/utils/ink.ts
+++ src/mappings/utils/ink.ts
@@ -2,36 +2,42 @@
 import { ContractsContractEmittedEvent } from '@subsquid/substrate-processor'
 import { DataSelection } from '@subsquid/substrate-processor/lib/interfaces/dataSelection'
 import * as psp34 from '../../abi/psp34'
+import { Interaction } from '../../model'
+import { possibleAddress } from './helper'
 // import { Context, EvmLogHandlerOptions, Interaction } from './types'
 // import { EvmLog, getEvmLog } from '@subsquid/ink-abi'
 
-
+const EMPTY_ADDRESS = ''
 export type RealEvent = psp34.Event
+export type Transfer = psp34.Event_Transfer
+export type Approval = psp34.Event_Approval
 
 
-// export const isMint = (addrOne: string, addrTwo: string) => {
-//   return addrOne === EMPTY_ADDRESS && addrTwo !== EMPTY_ADDRESS
-// }
+export const isMint = (addrOne: string, addrTwo: string): boolean => {
+  return addrOne === EMPTY_ADDRESS && addrTwo !== EMPTY_ADDRESS
+}
 
-// export const isBurn = (addrOne: string, addrTwo: string) => {
-//   return addrTwo === EMPTY_ADDRESS && addrOne !== EMPTY_ADDRESS
-// }
+export const isBurn = (addrOne: string, addrTwo: string): boolean => {
+  return addrTwo === EMPTY_ADDRESS && addrOne !== EMPTY_ADDRESS
+}
 
-// export const isTransfer = (addrOne: string, addrTwo: string) => {
-//   return !(isMint(addrOne, addrTwo) || isBurn(addrOne, addrTwo))
-// }
+export const isTransfer = (addrOne: string, addrTwo: string): boolean => {
+  return !(isMint(addrOne, addrTwo) || isBurn(addrOne, addrTwo))
+}
 
-// export const whatIsThisTransfer = (transfer: RealTransferEvent): Interaction => {
-//   const { from, to } = transfer
-//   if (isMint(from, to)) {
-//     return Interaction.MINTNFT
-//   }
-//   if (isBurn(from, to)) {
-//     return Interaction.CONSUME
-//   }
+export const whatIsThisTransfer = (transfer: Transfer): Interaction => {
+  const from = possibleAddress(transfer.from)
+  const to = possibleAddress(transfer.to)
+  
+  if (isMint(from, to)) {
+    return Interaction.MINTNFT
+  }
+  if (isBurn(from, to)) {
+    return Interaction.CONSUME
+  }
 
-//   return Interaction.SEND
-// }
+  return Interaction.SEND
+}
 
 export function decodeEvent(item: ContractsContractEmittedEvent): psp34.Event {
  return psp34.decodeEvent(item.args.data)
