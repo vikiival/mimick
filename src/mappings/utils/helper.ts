387a63f2edcc070b8415aa4aea39ef3c0f8a7959
Viki Val <viktorko99@gmail.com>
2022-12-14 16:21:17 +0100

    :soap: linter

--- src/mappings/utils/helper.ts
+++ src/mappings/utils/helper.ts
@@ -1,19 +1,21 @@
-import * as ss58 from '@subsquid/ss58';
-import { decodeHex } from '@subsquid/substrate-processor';
-import { NftId, SomethingWithOptionalMeta } from './types';
+import * as ss58 from '@subsquid/ss58'
+import { decodeHex } from '@subsquid/substrate-processor'
+import { NftId, SomethingWithOptionalMeta } from './types'
 
 export function isEmpty(obj: Record<string, unknown>): boolean {
   // eslint-disable-next-line guard-for-in, @typescript-eslint/naming-convention
-  for (const _ in obj) { return false; }
-  return true;
+  for (const _ in obj) {
+    return false
+  }
+  return true
 }
 
 export function addressOf(address: Uint8Array | string): string {
-  const value = typeof address === 'string' ? decodeHex(address) : address;
+  const value = typeof address === 'string' ? decodeHex(address) : address
   if (!value) {
-    return '';
+    return ''
   }
-  return ss58.codec(5).encode(value);
+  return ss58.codec(5).encode(value)
 }
 
 export function possibleAddress(address?: Uint8Array | string | null): string {
@@ -21,23 +23,23 @@ export function possibleAddress(address?: Uint8Array | string | null): string {
 }
 
 export function asNftId([collection, nft]: [number, number]): NftId {
-  return `${collection}-${nft}`;
+  return `${collection}-${nft}`
 }
 
 export function camelCase(str: string): string {
-	return str.replace(/([_][a-z])/gi, ($1) => {
-		return $1.toUpperCase().replace('_', '')
-	})
+  return str.replace(/([_][a-z])/gi, ($1) => {
+    return $1.toUpperCase().replace('_', '')
+  })
 }
 
 export function metadataOf({ metadata }: SomethingWithOptionalMeta): string {
-  return metadata ?? '';
+  return metadata ?? ''
 }
 
 export function oneOf<T>(one: T, two: T): T {
-  return one || two;
+  return one || two
 }
 
 export function toPercent(value: number): number {
-  return value / 100;
+  return value / 100
 }
