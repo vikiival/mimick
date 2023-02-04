5e324d795a4c12ce4d3fb1d468f3b868fde83cce
Viki Val <viktorko99@gmail.com>
2023-02-04 11:31:10 +0100

    :zap: logically extract block data

--- src/mappings/utils/extract.ts
+++ src/mappings/utils/extract.ts
@@ -4,14 +4,16 @@ import {
   ContractsContractEmittedEvent
 } from '@subsquid/substrate-processor'
 
+import { BaseBlock } from '@kodadot1/metasquid/types'
 import { CONTRACT_ADDRESS } from '../../constants'
 import { addressOf } from './helper'
 import { decodeEvent, RealEvent } from './ink'
 import logger from './logger'
 import { matchNFTEvent } from './matcher'
-import { BaseCall, Context, Processor } from './types'
+import { BaseCall, Context, Processor, WithBlock } from './types'
 
 type BaseEvent = BaseCall & RealEvent
+type MagicItem = Processor & WithBlock
 
 export function metaHandler(ctx: Context): BaseEvent[] {
   return ctx.blocks
@@ -23,29 +25,34 @@ export function metaHandler(ctx: Context): BaseEvent[] {
     .flat()
 }
 
-function enhanceItems(items: Processor[], baseBlock: BaseCall): BaseEvent[] {
+function enhanceItems(items: Processor[], baseBlock: BaseBlock): BaseEvent[] {
+  const enhanceWithBlock = addBlock(baseBlock)
   // eslint-disable-next-line id-length, @typescript-eslint/no-unsafe-return
   return items
     .filter(isEvent)
+    .map(enhanceWithBlock)
     .map(toBaseEvent)
     .filter(notEmpty)
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     .map((e) => ({ ...baseBlock, ...e }))
 }
 
+function addBlock<T = Processor>(block: BaseBlock): (item: T) => T & WithBlock {
+  return (item: T) => ({ ...item, block })
+}
+
 function notEmpty<T>(value: T | null | undefined): value is T {
   return value !== null && value !== undefined
 }
 
-function toBase(ctx: BatchBlock<Processor>): BaseCall {
-  const caller = ''
+function toBase(ctx: BatchBlock<Processor>): BaseBlock {
   const blockNumber = ctx.header.height.toString()
   const timestamp = new Date(ctx.header.timestamp)
 
-  return { caller, blockNumber, timestamp }
+  return { blockNumber, timestamp }
 }
 
-function toBaseEvent(ctx: Processor): any {
+function toBaseEvent(ctx: MagicItem): any {
   logger.info(`[BASE EVENT]`, ctx)
   if (ctx.name === 'Contracts.ContractEmitted') {
     logger.info(
