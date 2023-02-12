70fa85160d1503b4e61803bf3bb8ecadfbc21702
Viki Val <viktorko99@gmail.com>
2023-02-12 22:47:23 +0100

    :construction: Something

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,11 +1,17 @@
+import { toUniqueSet } from '@kodadot1/metasquid'
+import { findByIdListAsMap } from '@kodadot1/metasquid/entity'
 // import { unwrap } from '../utils/extract'
 import { metaHandler } from './utils/extract'
 import logger from './utils/logger'
 import { serializer } from './utils/serializer'
 import { Context } from './utils/types'
+import { NFTEntity } from '../model'
 
 export async function mainFrame(context: Context): Promise<void> {
   const events = metaHandler(context)
+
+  const idSet = toUniqueSet(events)
+  const entities = await findByIdListAsMap(context.store, NFTEntity, idSet)
 
   
 
