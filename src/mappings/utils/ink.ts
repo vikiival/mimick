1500422b5160e19417f5ccac3defa8a003033720
Viki Val <viktorko99@gmail.com>
2024-09-05 14:59:48 +0200

    Update ink.ts with ERC721 event types and address helper functions

--- src/mappings/utils/ink.ts
+++ src/mappings/utils/ink.ts
@@ -1,15 +1,12 @@
-import { ContractsContractEmittedEvent } from '@subsquid/substrate-processor'
-import { DataSelection } from '@subsquid/substrate-processor/lib/interfaces/dataSelection'
-import * as psp34 from '../../abi/psp34'
+import * as erc721 from '../../abi/erc721'
 import { Interaction } from '../../model'
-import { possibleAddress } from './helper'
-// import { Context, EvmLogHandlerOptions, Interaction } from './types'
-// import { EvmLog, getEvmLog } from '@subsquid/ink-abi'
+import { addressOf } from './helper'
+import { debug } from 'console'
 
-const EMPTY_ADDRESS = ''
-export type RealEvent = psp34.Event
-export type Transfer = psp34.Event_Transfer
-export type Approval = psp34.Event_Approval
+const EMPTY_ADDRESS = '6nfFzYQPHsjA4QTuXt2NuGReV9J4BdXBwZ8cgeJTrbVHShE'
+export type RealEvent = erc721.Event
+export type Transfer = erc721.Event_Transfer
+export type Approval = erc721.Event_Approval
 
 export const isMint = (addrOne: string, addrTwo: string): boolean => {
   return addrOne === EMPTY_ADDRESS && addrTwo !== EMPTY_ADDRESS
@@ -24,25 +21,22 @@ export const isTransfer = (addrOne: string, addrTwo: string): boolean => {
 }
 
 export const whatIsThisTransfer = (transfer: Transfer): Interaction => {
-  const from = possibleAddress(transfer.from)
-  const to = possibleAddress(transfer.to)
+  const from = addressOf(transfer.from)
+  const to = addressOf(transfer.to)
+
+  debug('whatIsThisTransfer' as any, { from, to })
 
   if (isMint(from, to)) {
-    return Interaction.MINTNFT
+    return Interaction.MINT
   }
   if (isBurn(from, to)) {
-    return Interaction.CONSUME
+    return Interaction.BURN
   }
 
   return Interaction.SEND
 }
 
-export function decodeEvent(item: ContractsContractEmittedEvent): psp34.Event {
-  return psp34.decodeEvent(item.args.data)
+export function decodeEvent(item: any, topics: any): erc721.Event {
+  return erc721.decodeEvent(item, topics)
 }
 
-export const contractFilter: DataSelection<{ event: { args: boolean } }> = {
-  data: {
-    event: { args: true },
-  },
-}
